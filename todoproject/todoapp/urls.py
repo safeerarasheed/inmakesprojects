257ebe99c59fda3760a8612a18748e6e89fc9ba0
safeerarasheed <safrash2016@gmail.com>
2022-12-01 20:02:57 +0530

    added

---
+++ todoproject/todoapp/urls.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.add, name='add'),
+    path('delete/<int:taskid>/', views.delete, name='delete'),
+    path('update/<int:id>/', views.update, name='update'),
+    path('cbvhome/', views.Tasklistview.as_view(), name='cbvhome'),
+    path('cbvdetail/<int:pk>/', views.Taskdetailview.as_view(), name='cbvdetail'),
+    path('cbvupdate/<int:pk>/', views.Taskupdateview.as_view(), name='cbvupdate'),
+    path('cbvdelete/<int:pk>/', views.Taskdeleteview.as_view(), name= 'cbvdelete'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
